dbd8765e53e645484fec035549b69d064b703705
S525703 <S525703@NW33108.nwmissouri.edu>
2016-09-15 09:50:05 -0500

    hi

---
+++ Calculotor/src/calculotor/Calculotor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculotor;
+
+/**
+ *
+ * @author S525703
+ */
+public class Calculotor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hi");
+        System.out.println("hello");
+               
+    }
+    
+}
